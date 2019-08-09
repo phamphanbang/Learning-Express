c8c7db123c7bc197c56d273c2192a6bc30e07e95
phamphanbang <phamphanbang@gmail.com>
2019-08-09 21:49:12 +0700

    Make a POST request to create a new user

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+
 var port = 3000;
+
+var users = [
+    {id: 1, name: 'Bang'},
+    {id: 2, name: 'Goal'}
+];
 
 app.get('/', (req, res) => {
     res.render('index', {
@@ -15,11 +24,28 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.render('users/index', {
-        users : [
-            {id: 1, name: 'Bang'},
-            {id: 2, name: 'Goal'}
-        ]
+        users : users
     });
 });
+
+app.get('/users/search', (req ,res) => {
+    var q = req.query.q;
+    var matchUser = users.filter( (user) => {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+
+    res.render('users/index', {
+        users : matchUser
+    });
+});
+
+app.get('/users/create', (req, res) => {
+    res.render('users/create');
+})
+
+app.post('/users/create', (req, res) => {
+    users.push(req.body);
+    res.redirect('/users');
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
