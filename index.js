06647ebc369c9dcd57c6396dd6e015f4bb7e7f45
phamphanbang <phamphanbang@gmail.com>
2019-08-12 15:43:40 +0700

    Learning Express routes

--- index.js
+++ index.js
@@ -1,26 +1,19 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-var shortid = require('shortid');
 
-
-db = low(adapter);
-
-db.defaults({ users: []})
-  .write()
+var userRoute = require('./routes/user.route');
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 var port = 3000;
 
-
+app.use('/users', userRoute);
 
 app.get('/', (req, res) => {
     res.render('index', {
@@ -28,42 +21,6 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/users', (req, res) => {
-    res.render('users/index', {
-        users : db.get('users').value()
-    });
-});
 
-app.get('/users/search', (req ,res) => {
-    var q = req.query.q;
-    var users = db.get('users').value();
-    var matchUser = users.filter( (user) => {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-
-    res.render('users/index', {
-        users : matchUser
-    });
-});
-
-app.get('/users/create', (req, res) => {
-    res.render('users/create');
-})
-
-app.get('/users/:id', (req, res) =>{
-    var id = req.params.id;
-
-    var user = db.get('users').find({id : id}).value();
-
-    res.render('users/view', {
-        user : user 
-    })
-})
-
-app.post('/users/create', (req, res) => {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect('/users');
-})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
